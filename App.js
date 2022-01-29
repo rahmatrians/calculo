56711f1ff53572966846c48476ca92c3a1232bea
Rahmat Rians <rahmatriansyah57@gmail.com>
2022-01-29 12:48:42 +0700

    fixing formula logic

--- App.js
+++ App.js
@@ -19,6 +19,7 @@ const App = () => {
   const [tempVal, setTempVal] = useState('');
   const [lastResult, setLastResult] = useState(0);
   const [getOperator, setGetOperator] = useState('');
+  const [test, setTest] = useState([]);
 
   // useEffect(() => {
   // });
@@ -29,90 +30,132 @@ const App = () => {
   }
   
   const removeOne = () => {
-    value.pop();
+    // if (tempVal !== '') {
+    //   value.push(parseInt(tempVal));
+    //   valueHistory.push(parseInt(tempVal));
+    //   setTempVal('');
+    // }
+
+    // console.log("first => removed : ", value, "number : ", number);
+    
+    // setTest(value[value.length-1]);
+    // setTest(test.slice(0,-1));
+    // console.log("item last of value 1 : ", data);
+    // console.log("item last of value 2 : ", test);
+    
+    // setValue(value.splice(value.length-1, 1, parseInt(data)));
     setNumber(number.slice(0,-1));
-    console.log("removed");
+    console.log("last => removed : ", test, "number : ", number);
+  }
+  
+  const checkTest = () => {
+    // let indexes = [number.split('/').join(',').split('x').join(',').split('-').join(',').split('+').join(',')];
+    let indexes = number.split(/([ \/, \+, \-, \x])+/g);
+
+    console.log(number);
+    console.log(indexes);
+
+    // for (let i = 0; i < number.length; i++) {
+      // if (number[i] === '/' || number[i] === 'x' || number[i] === '-' || number[i] === '+') {
+      //   indexes.push(i);
+      // }
+    // }
+    // console.log(indexes);
+    
+
+    // console.log("vals : ", getOperator, "number : ", number);
   }
 
   const operator = (opr) => {
-    if (tempVal) {
-      value.push(parseInt(tempVal));
-    }
-    setTempVal('');
-    value.push(opr);
+    // if (tempVal) {
+    //   value.push(parseInt(tempVal));
+    // }
+    // setTempVal('');
+    // value.push(opr);
     setNumber(number + opr);
-    console.log(value);
+    // console.log(value);
+  }
+
+  const calcualteTimes = () => {
   }
   
-  const equalVal = () => {
-    var y = 0;
-    const operators = ['/','x','-','+'];
-    if (tempVal) {
-      value.push(parseInt(tempVal));
-      valueHistory.push(parseInt(tempVal));
-      setTempVal('');
-    }
-    console.log(value.length, "data:", value);
+  const equalVal = async () => {
+    setTempVal('');
+    console.log("tempval: ", tempVal);
+    console.log("number: ", number);
+    // if (tempVal) {
+    //   value.push(parseInt(tempVal));
+    //   valueHistory.push(parseInt(tempVal));
+    //   setTempVal('');
+    // }
+    // console.log(value.length, "data:", value);
     // value.forEach((value, index, array) => dataLoop(value, index, array));
-
-    // for (let x = 1; x <= ((operator.length-1)/2); x+2) {
-
-
-      
-    for (let x = (value.length-1)/2; x > 0 ; x--) {
+    
+    let indexes = await number.split(/([ \/, \+, \_, \x])+/g);
+    // await setValue(indexes);
+    
+    for (let x = (indexes.length-1)/2; x > 0 ; x--) {
       var datax = [];
-      value.filter((vals, idx) => (vals === '/' || vals === 'x') && (datax.push(idx)));
+      indexes.filter((vals, idx) => (vals === '/' || vals === 'x') && (datax.push(idx)));
       console.log("datax : ", datax);
-      if (datax.length === 0) {
-        break;        
-      } else {
-        console.log("value:", value);
+      if (datax.length !== 0) {
+        console.log("indexes:", indexes);
         console.log("operator :", datax[0]);
 
-        if (value[datax[0]] === '/') {
-          var data =  value[datax[0]-1] / value[datax[0]+1];
+        if (indexes[datax[0]] === '/') {
+          var data =  parseInt(indexes[datax[0]-1]) / parseInt(indexes[datax[0]+1]);
           console.log("data : ", data);
-          value.splice(datax[0]-1, 1, data);
-          // console.log(value[datax[0]-1], "/", value[datax[0]+1]);
-        } else if (value[datax[0]] === 'x') {
-          var data =  value[datax[0]-1] * value[datax[0]+1];
+          indexes.splice(datax[0]-1, 1, data);
+          // console.log(indexes[datax[0]-1], "/", indexes[datax[0]+1]);
+        } else if (indexes[datax[0]] === 'x') {
+          var data =  parseInt(indexes[datax[0]-1]) * parseInt(indexes[datax[0]+1]);
           console.log("data : ", data);
-          value.splice(datax[0]-1, 1, data);
-          // console.log(value[datax[0]-1], "*", value[datax[0]+1]);
+          indexes.splice(datax[0]-1, 1, data);
+          // console.log(indexes[datax[0]-1], "*", indexes[datax[0]+1]);
         } else {
           console.log("e");
         }
-        value.splice(datax[0], 2);
-        console.log("valss : ", value);
+        indexes.splice(datax[0], 2);
+        console.log("valss : ", indexes);
+        setValue(indexes);
+        setNumber(''+indexes);
+      }else {
+        break;
       }
     }
 
     console.log("============================================================");
 
-    for (let x = (value.length-1)/2; x > 0 ; x--) {
-      var dataz = [];
-      value.filter((vals, idx) => (vals === '-' || vals === '+') && (dataz.push(idx)));
-      console.log("dataz : ", dataz);
-      console.log("valuez : ", value);
-      console.log("operatorz : ", dataz[0]);
+    for (let x = (indexes.length-1)/2; x > 0 ; x--) {
+      var datax = [];
+      indexes.filter((vals, idx) => (vals === '_' || vals === '+') && (datax.push(idx)));
+      console.log("datax : ", datax);
+      if (datax.length !== 0) {
+        console.log("indexes:", indexes);
+        console.log("operator :", datax[0]);
 
-      if (value[dataz[0]] === '-') {
-        var data =  value[dataz[0]-1] - value[dataz[0]+1];
-        console.log("data : ", data);
-        value.splice(dataz[0]-1, 1, data);
-        // console.log(value[dataz[0]-1], "/", value[dataz[0]+1]);
-      } else if (value[dataz[0]] === '+') {
-        var data =  value[dataz[0]-1] + value[dataz[0]+1];
-        console.log("data : ", data);
-        value.splice(dataz[0]-1, 1, data);
-        // console.log(value[dataz[0]-1], "*", value[dataz[0]+1]);
-      } else {
-        console.log("e");
+        if (indexes[datax[0]] === '_') {
+          var data =  parseInt(indexes[datax[0]-1]) - parseInt(indexes[datax[0]+1]);
+          console.log("data : ", data);
+          indexes.splice(datax[0]-1, 1, data);
+          // console.log(indexes[datax[0]-1], "/", indexes[datax[0]+1]);
+        } else if (indexes[datax[0]] === '+') {
+          var data =  parseInt(indexes[datax[0]-1]) + parseInt(indexes[datax[0]+1]);
+          console.log("data : ", data);
+          indexes.splice(datax[0]-1, 1, data);
+          // console.log(indexes[datax[0]-1], "*", indexes[datax[0]+1]);
+        } else {
+          console.log("e");
+        }
+        indexes.splice(datax[0], 2);
+        console.log("valss : ", indexes);
+        setValue(indexes);
+        setNumber(''+indexes);
+      }else {
+        break;
       }
-      value.splice(dataz[0], 2);
-      console.log("valss : ", value);
-      setNumber(value);
     }
+
   }
  
 
@@ -130,7 +173,7 @@ const App = () => {
         <View style={{ backgroundColor:'#2a2d37', flex:4, flexDirection:'column', padding:20}}>
           <View style={{ backgroundColor:'#2a2d37', flexDirection:'row', alignContent:'space-between'}}>
             <TouchableOpacity onPress={() => removeOne()} style={{ padding:20, margin:7, borderRadius:25,backgroundColor:'#282b33', flex:1 }}><Text style={{ textAlign:'center', color:'#31dcbe', fontSize:28 }}>C</Text></TouchableOpacity>
-            <TouchableOpacity onPress={() => operator("_")} style={{ padding:20, margin:7, borderRadius:25,backgroundColor:'#282b33', flex:1 }}><Text style={{ textAlign:'center', color:'#31dcbe', fontSize:28 }}>_</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => checkTest()} style={{ padding:20, margin:7, borderRadius:25,backgroundColor:'#282b33', flex:1 }}><Text style={{ textAlign:'center', color:'#31dcbe', fontSize:28 }}>_</Text></TouchableOpacity>
             <TouchableOpacity onPress={() => operator("%")} style={{ padding:20, margin:7, borderRadius:25,backgroundColor:'#282b33', flex:1 }}><Text style={{ textAlign:'center', color:'#31dcbe', fontSize:28 }}>%</Text></TouchableOpacity>
             <TouchableOpacity onPress={() => operator("/")} style={{ padding:20, margin:7, borderRadius:25,backgroundColor:'#282b33', flex:1 }}><Text style={{ textAlign:'center', color:'#d16365', fontSize:28 }}>/</Text></TouchableOpacity>
           </View>
@@ -144,7 +187,7 @@ const App = () => {
             <TouchableOpacity onPress={() => addValue(4)} style={{ padding:20, margin:7, borderRadius:25,backgroundColor:'#282b33', flex:1 }}><Text style={{ textAlign:'center', color:'white', fontSize:28 }}>4</Text></TouchableOpacity>
             <TouchableOpacity onPress={() => addValue(5)} style={{ padding:20, margin:7, borderRadius:25,backgroundColor:'#282b33', flex:1 }}><Text style={{ textAlign:'center', color:'white', fontSize:28 }}>5</Text></TouchableOpacity>
             <TouchableOpacity onPress={() => addValue(6)} style={{ padding:20, margin:7, borderRadius:25,backgroundColor:'#282b33', flex:1 }}><Text style={{ textAlign:'center', color:'white', fontSize:28 }}>6</Text></TouchableOpacity>
-            <TouchableOpacity onPress={() => operator("-")} style={{ padding:20, margin:7, borderRadius:25,backgroundColor:'#282b33', flex:1 }}><Text style={{ textAlign:'center', color:'#d16365', fontSize:28 }}>-</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => operator("_")} style={{ padding:20, margin:7, borderRadius:25,backgroundColor:'#282b33', flex:1 }}><Text style={{ textAlign:'center', color:'#d16365', fontSize:28 }}>-</Text></TouchableOpacity>
           </View>
           <View style={{ backgroundColor:'#2a2d37', flexDirection:'row', alignContent:'space-between'}}>
             <TouchableOpacity onPress={() => addValue(1)} style={{ padding:20, margin:7, borderRadius:25,backgroundColor:'#282b33', flex:1 }}><Text style={{ textAlign:'center', color:'white', fontSize:28 }}>1</Text></TouchableOpacity>
